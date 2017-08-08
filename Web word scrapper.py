7f219554aff51630699366c0cd063d1fec78d09d
bikz007 <bikrammodak75@gmail.com>
2017-08-08 17:09:28 +0530

    Update Web word scrapper.py

--- Web word scrapper.py
+++ Web word scrapper.py
@@ -20,6 +20,16 @@ def clean_up_list(word_list):
 		for i in range(0,len(symbols)):
 			word=word.replace(symbols[i],"") #to replace special symbols
 		if len(word)>0: #if lenth of word is less than 0 then it is not a word
-			print(word)
 			clean_word_list.append(word) #add words to list
+	create_dictionary(clean_word_list)
+def create_dictionary(clean_word_list):
+	word_count={} #a dictionary where we will store the words and their frequencies as key
+	for word in clean_word_list :
+		if word in word_count: #if the word is already present 
+			word_count[word]+=1 #increase its no. of occurance
+		else:
+			word_count[word]=1 #keep it in
+	#to sort according to frequencies
+	for key,value in sorted(word_count.items(),key=operator.itemgetter(1)): #in a dictionary key is the word and value will be its frequency,so getting key from dictionary and specifying to sort according to value
+		print(key,value)
 start('https://forums.digitalspy.com/discussion/2235016/in-large-debt-tell-my-parents')
